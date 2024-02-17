8c9d874a25298762add40e3883dc121082cd3038
Willy NOUBISSIE <willynoubissie@MBP-de-Willy.lan>
2024-02-17 22:16:30 +0100

    udpate responsive

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,48 +1,61 @@
-'use client';
+"use client";
 
-import { signIn, signOut, useSession } from 'next-auth/react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export function AppNavbar() {
-    const { data } = useSession();
-    const [user, setUser] = useState<string | null>(null);
-    useEffect(() => {
-        if (data && data.user && data.user.name) {
-            setUser(data.user.name);
-        }
-    },[data]);
+  const { data } = useSession();
+  const [user, setUser] = useState<string | null>(null);
+  useEffect(() => {
+    if (data && data.user && data.user.name) {
+      setUser(data.user.name);
+    }
+  }, [data]);
 
   return (
-    <Navbar expand="lg" bg='pb-light'>
+    <Navbar expand="lg" bg="pb-light">
       <Container>
-        <Navbar.Brand href="/" className='justify-content-between'>
-            <Image src="/logo.png" alt="logo" width="30" height="30" className="d-inline-block align-top" />
+        <Navbar.Brand href="/" className="justify-content-between">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Accueil</Nav.Link>
             <Nav.Link href="/avaibilities">Mes disponibilités</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
-        {
-            user ? 
+          {user ? (
             <NavDropdown title={user} id="basic-nav-dropdown">
-                <NavDropdown.Item><Link href='/profile'>Mon profil</Link></NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item onClick={() => signOut()}>Déconnexion</NavDropdown.Item>
-            </NavDropdown> :
+              <NavDropdown.Item>
+                <Link href="/profile">Mon profil</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => signOut()}>
+                Déconnexion
+              </NavDropdown.Item>
+            </NavDropdown>
+          ) : (
             <Nav.Item>
-                <Button variant="pb-primary" onClick={() => signIn('keycloak')}>Connexion</Button>
+              <Button variant="pb-primary" onClick={() => signIn("keycloak")}>
+                Connexion
+              </Button>
             </Nav.Item>
-        }
+          )}
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
