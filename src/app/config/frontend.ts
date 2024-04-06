730024171027d63ab5f6911588798c631c57e636
Willy NOUBISSIE <willyjunior91@gmail.com>
2024-04-06 19:01:40 +0200

    remove console.log

--- src/app/config/frontend.ts
+++ src/app/config/frontend.ts
@@ -16,8 +16,6 @@ export function setRouter(
 }
 
 export const frontendConfig = (): SuperTokensConfig => {
-  console.log("frontendConfig, appInfo: ", appInfo);
-  console.log(" process.env ", process.env.NEXT_PUBLIC_SUPERTOKENS_API_DOMAIN)
   return {
     appInfo,
     recipeList: [
